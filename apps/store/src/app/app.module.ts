5aaa725e9b971c4493528aed09b93fdd9a124b2a
KaterynaZorina <kateryna@zwapgrid.com>
2021-01-31 17:32:27 +0200

    Lab 4

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { StoreUiSharedModule } from 'libs/store/ui-shared/src/lib/store-ui-shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-    MatCardModule
+    MatCardModule,
+    StoreUiSharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
